0a6dfeb5ec20863b8e9043f8c983fadcdfabaff1
Alexey <koroza.alexey99@gmail.com>
2022-03-05 17:15:32 +0300

    *

--- partTwo/numberFive.js
+++ partTwo/numberFive.js
@@ -3,11 +3,11 @@
     Вывести в консоль с помощью цикла WHILE все нечетные числа от 21 до 67
  */
 
-    let number = 21;
-    
-    while(number < 67){
-        if (number % 2 != 0){
-            console.log(number);
-        }
-        number++
-    }
+let number = 21;
+
+while (number < 67) {
+  if (number % 2 != 0) {
+    console.log(number);
+  }
+  number++;
+}
