519ac96c9e55274ae02462e55668a6284da0cab8
Alexey <koroza.alexey99@gmail.com>
2022-03-08 18:12:18 +0300

    *

--- DOM/main.js
+++ DOM/main.js
@@ -1,4 +1,21 @@
 //document object model (DOM)
 
-const html = document.getElementById('js');
-console.log(html);
+/* const div = document.getElementById('div'); */
+const btn = document.getElementById('btn');
+
+/* btn.addEventListener('click', () => {
+     div.classList.toggle('hide'); 
+    div.style.fontSize = '50px';
+    div.style.color = 'red'
+}); */
+
+const showMessage = () => {
+    alert('Ты нажал на кнопку');
+}
+
+btn.onclick = showMessage;
+btn.addEventListener('click', () => {
+    showMessage();
+})
+
+console.log(btn)
