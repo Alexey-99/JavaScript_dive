dcbd6e1c49e371bc77cbe8780cace8e5be8e15a1
Alexey <koroza.alexey99@gmail.com>
2022-03-08 19:27:47 +0300

    *

---
+++ tabs/main.js
@@ -0,0 +1,16 @@
+const tabs = document.getElementById('tabs');
+const content = document.querySelectorAll('.content');
+
+/* console.log(tabs.children); */
+
+const changeClass = (el) => {
+    for(let i = 0; i < tabs.children.length; i++){
+        tabs.children[i].classList.remove('active');
+    }
+    el.classList.add('active');
+}
+
+tabs.addEventListener('click', (e) => {
+  const currTab = e.target.dataset.btn;
+  changeClass(e.target);
+});
